c2c8318e90851555ec2072d819e8e1fe053de396
ViperTraineeXYZ <ahmedabidi98@gmail.com>
2025-06-08 13:40:51 +0100

    refactor: simplify navbar structure and update styles for better readability

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
